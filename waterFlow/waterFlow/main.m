30ee3a29c1ce5b52ca57d08e2aca829d88ab00f5
LDG <ldg@LDGdeMacBook-Pro.local>
2015-12-28 19:04:24 +0800

    初始化仓库

---
+++ waterFlow/waterFlow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  waterFlow
+//
+//  Created by LDG on 15/12/28.
+//  Copyright © 2015年 ldg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
