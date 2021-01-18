c3113a1bb3db887b503b75cd7225bdc25aabbf61
ludg <792074627@qq.com>
2021-01-18 11:28:57 +0800

    addfile

---
+++ waterFlow/waterFlow/testViewController.h
@@ -0,0 +1,17 @@
+//
+//  testViewController.h
+//  waterFlow
+//
+//  Created by 卢大刚 on 2021/1/18.
+//  Copyright © 2021 ldg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface testViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ waterFlow/waterFlow/testViewController.m
@@ -0,0 +1,32 @@
+//
+//  testViewController.m
+//  waterFlow
+//
+//  Created by 卢大刚 on 2021/1/18.
+//  Copyright © 2021 ldg. All rights reserved.
+//
+
+#import "testViewController.h"
+
+@interface testViewController ()
+
+@end
+
+@implementation testViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
